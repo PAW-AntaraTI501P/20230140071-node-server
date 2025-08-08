cf3e9ce57ee616eb3bc1db90717e4dc3905988b7
Dhnnz12 <dhonanhibrizi123@gmail.com>
2025-08-08 10:18:07 +0700

    pertemuan5

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 3001;
+const port = process.env.PORT || 3001;
+const { router: todoRoutes, todos } = require("./routes/todo.js");
+
 app.use(cors());
-
-
-const todoRoutes = require("./routes/todo");
-const { todos } = require("./routes/todo");
-
 app.use(express.json());
+app.set("view engine", "ejs");
+
 app.use("/todos", todoRoutes);
 
-app.set("view engine", "ejs");
+app.get("/todos-data", (req, res) => {
+  res.json(todos);
+});
+
+app.get("/todos-list", (req, res) => {
+  res.render("todos-page", { todos: todos });
+});
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -22,9 +28,8 @@ app.get("/contact", (req, res) => {
 });
 
 app.use((req, res) => {
-  res.status(404).send("Page not found");
+  res.status(404).send("404 - Page Not Found");
 });
-
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
